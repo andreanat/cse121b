d6236640042a8bfe0e70f83a4e78cd0f5c413fba
andreanat <andrea.nmb13@gmail.com>
2023-09-20 11:31:27 -0600

    s

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -11,7 +11,7 @@ const profilePicture = 'images/andrea-headshot.jpg';
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById('name');
 const yearElement = document.querySelector('#year');
-const imageElement = document.getElementById('profileImage');
+const imageElement = document.getElementById('profilePicture');
 const foodElement = document.getElementById('food');
 
 /* Step 4 - Adding Content */
