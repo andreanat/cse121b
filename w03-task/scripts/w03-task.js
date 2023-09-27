aed2cc7050037924b3e76f0e1a7abc8befbe944e
andreanat <andrea.nmb13@gmail.com>
2023-09-26 18:43:10 -0600

    8

--- w03-task/scripts/w03-task.js
+++ w03-task/scripts/w03-task.js
@@ -56,6 +56,7 @@ document.getElementById("year").value = currentYear;
 /* ARRAY METHODS - Functional Programming */
 /* Output Source Array */
 let numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+document.querySelector('#array').innerHTML = numbersArray.join(', ');
 
 /* Output Odds Only Array */
 document.querySelector('#odds').innerHTML = numbersArray.filter(number => number % 2 !== 0).join(', ');
