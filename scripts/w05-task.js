0c5f5cfbf0133cb1e8de89a7a0b6376de2b0f3cd
andreanat <andrea.nmb13@gmail.com>
2023-10-12 19:51:35 -0600

    mm

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -3,67 +3,69 @@
 /* Declare and initialize global variables */
 const templesElement = document.getElementById("templesElement");
 const templeList = [];
+
 /* async displayTemples Function */
-let articleElement = document.createElement("article");
+const displayTemples = (temples) => {
+    reset();
+    temples.forEach((temple) => {
+        let articleElement = document.createElement("article");
 
-let h3Element = document.createElement("h3");
-h3Element.textContent = templeList.templeName;
+        let h3Element = document.createElement("h3");
+        h3Element.textContent = temple.templeName;
 
-let imgElement = document.createElement("img");
-imgElement.src = temple.imageUrl;
-imgElement.alt = temple.location;
+        let imgElement = document.createElement("img");
+        imgElement.src = temple.imageUrl;
+        imgElement.alt = temple.location;
 
-articleElement.appendChild(h3Element);
-articleElement.appendChild(imgElement);
-templesElement.appendChild(articleElement);
+        articleElement.appendChild(h3Element);
+        articleElement.appendChild(imgElement);
+        templesElement.appendChild(articleElement);
+    });
+};
 
 /* async getTemples Function using fetch()*/
 const getTemples = async () => {
-    const response = await fetch ("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+    const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
     const data = await response.json();
     data.forEach((temple) => {
         templeList.push(temple);
     });
     displayTemples(templeList);
     console.log(templeList);
-}
+};
 
 /* reset Function */
-let reset = function() {
-    const articles = templesElement.querySelectorAll('article');
-    articles.forEach((article) => {
-        templesElement.removeChild(article);
-    });
+const reset = () => {
+    while (templesElement.firstChild) {
+        templesElement.removeChild(templesElement.firstChild);
+    }
 };
-
-/* sortBy Function */
-const sortBy = function(temples){
-};
-reset();
-const filter = document.getElementById("sortBy").value;
-switch (filter) {
-    case "utah":
-        const utahTemples = temples.filter(temp => temp.location.includes("Utah"));
-        displayTemples(utahTemples);
-        break;
-
-    case "nonutah":
-        const nonUtahTemples = temples.filter(temp => !temp.location.includes("Utah"));
-        displayTemples(nonUtahTemples);
-        break;
-
-    case "older":
-        const olderTemples = temples.filter(temp => new Date(temp.dedicated) < new Date(1950, 0, 1));
-        displayTemples(olderTemples);
-        break;
-
-    case "all":
-    default:
-        displayTemples(temples);
-        break;
-}
-
-getTemples();
 
 /* Event Listener */
-document.querySelector("#sortBy").addEventListener("change",() => { sortBy(templeList) });
+document.querySelector("#sortBy").addEventListener("change", () => {
+    const filter = document.getElementById("sortBy").value;
+
+    switch (filter) {
+        case "utah":
+            const utahTemples = templeList.filter((temp) => temp.location.includes("Utah"));
+            displayTemples(utahTemples);
+            break;
+
+        case "nonutah":
+            const nonUtahTemples = templeList.filter((temp) => !temp.location.includes("Utah"));
+            displayTemples(nonUtahTemples);
+            break;
+
+        case "older":
+            const olderTemples = templeList.filter((temp) => new Date(temp.dedicated) < new Date(1950, 0, 1));
+            displayTemples(olderTemples);
+            break;
+
+        case "all":
+        default:
+            displayTemples(templeList);
+            break;
+    }
+});
+
+getTemples();
